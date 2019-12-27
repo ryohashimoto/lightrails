c51618ec42531b5b12fd7719da841a834730834c
Ryo Hashimoto <ryohashimoto@gmail.com>
2019-12-27 09:16:09 +0900

    Add ActionInteractor::Errors

---
+++ actioninteractor/lib/action_interactor/errors.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require "forwardable"
+
+module ActionInteractor
+  # == Action \Interactor \Errors
+  # Provides a +Hash+ like object to Action Interactors execution errors.
+  class Errors
+    include Enumerable
+    extend Forwardable
+
+    attr_reader :errors
+
+    def_delegators :@errors, :clear, :keys, :values, :[], :delete
+
+    def initialize(*)
+      @errors = {}
+    end
+
+    # Add +error+ to the errors hash.
+    def add(attribute, error)
+      errors[attribute.to_sym] = error
+    end
+
+    # Delete a error for +key+.
+    def delete(key)
+      attribute = key.to_sym
+      errors.delete(attribute)
+    end
+
+    # Iterates through each error key, value pair in the errors hash.
+    def each
+      errors.each_key do |attribute|
+        yield attribute, errors[attribute]
+      end
+    end
+  end
+end
