3711935c1c30664205162df414b52dc56d17a44d
Ryo Hashimoto <ryohashimoto@gmail.com>
2019-12-28 13:49:32 +0900

    Add ActionInteractor::Base#facade_for method

--- actionfacade/lib/action_facade/retrieval.rb
+++ actionfacade/lib/action_facade/retrieval.rb
@@ -7,5 +7,17 @@ module ActionFacade
         instance_variable_set("@#{name}", facade.send(name.to_sym))
       end
     end
+
+    def facade_klass_name
+      params[:controller].gsub(/Controller$/, "::") + params[:action].classify
+    end
+
+    def facade_klass
+      facade_klass_name.constantize
+    end
+
+    def facade_for(payload)
+      facade_klass.new(payload)
+    end
   end
 end
