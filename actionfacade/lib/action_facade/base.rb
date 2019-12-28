ea45894a1a0eedabd4be318c5edc2c34a7df9729
Ryo Hashimoto <ryohashimoto@gmail.com>
2019-12-28 16:53:17 +0900

    Add comments to the ActionFacade class / module

--- actionfacade/lib/action_facade/base.rb
+++ actionfacade/lib/action_facade/base.rb
@@ -1,6 +1,31 @@
 # frozen_string_literal: true
 
 module ActionFacade
+  # == Action \Facade \Base
+  #
+  # This is only a base class for extract data from input (payload).
+  # This can be inherited to another class and you can implement
+  # some mothods for obtaining data.
+  # In Ruby on Rails, you can implemented Active Record query methods
+  # to the inherited class.
+  # For example
+  #
+  #   class Mypage::IndexFacade < ApplicationFacade
+  #     attr_reader :current_user
+  #
+  #     def initialize(params)
+  #       @current_user = params[:current_user]
+  #     end
+  #
+  #     def active_users
+  #       @active_users ||= User.active.order(login_at: :desc).limit(10)
+  #     end
+  #
+  #     def messages
+  #       @messages ||= current_user.messages.order(created_at: :desc).limit(10)
+  #     end
+  #   end
+  #
   class Base
     attr_reader :payload
 
--- actionfacade/lib/action_facade/retrieval.rb
+++ actionfacade/lib/action_facade/retrieval.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
 
 module ActionFacade
+  # == Action \Facade \Retrieval
+  #
+  # This module provides `retrieve` method to the class which includes the module.
+  # The `retrieve` method can be used for obtaining data from the external facade.
+  # In Ruby on Rails controllers, this module can be used for assing many instance
+  # variables by extracting Active Record queries to the class that is inherited
+  # from ActionFacade::Base.
   module Retrieval
+    # Retrieve data from `facade`
+    #
+    # `variable_names` are symbols for the method names in the facade and
+    # they will be set as instance variables in the class that
+    # includes the module.
     def retrieve(facade, *variable_names)
       variable_names.each do |name|
         instance_variable_set("@#{name}", facade.send(name.to_sym))
