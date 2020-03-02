992cc3a27e1009928b2f9ba59a129b960ba87263
Ryo Hashimoto <ryohashimoto@gmail.com>
2020-03-02 18:29:44 +0900

    Comments for ActionInteractor::State

--- actioninteractor/lib/action_interactor/state.rb
+++ actioninteractor/lib/action_interactor/state.rb
@@ -29,32 +29,38 @@ module ActionInteractor
       :initial
     end
 
-    # Returns true if transition to target_state from current state.
+    # Returns true if transition to target_state from current state
     def valid_transition?(target_state)
       transitions[target_state].include?(state)
     end
 
+    # Available states for the instance
     def states
       self.class.states
     end
 
+    # Avaiolable transitions for the instance's states
     def transitions
       self.class.transitions
     end
 
+    # Available states for the class
     def self.states
       STATES
     end
 
+    # Available transitions for the class
     def self.transitions
       TRANSITIONS
     end
 
     self.states.each do |state_name|
+      # Returns true if state_name is the same as current state
       define_method("#{state_name}?") do
         state == state_name
       end
 
+      # Set current state to the state_name, otherwise raises error
       define_method("#{state_name}!") do
         unless valid_transition?(state_name)
           raise TransitionError.new("Could not change state :#{state_name} from :#{state}")
@@ -63,6 +69,7 @@ module ActionInteractor
       end
     end
 
+    # Error for invalid transitions
     class TransitionError < StandardError; end
   end
 end
